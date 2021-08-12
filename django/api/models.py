0be9add4c81008317d32f0869ba3765cbee37f47
Rachele Morino <rmorino@azavea.com>
2021-08-12 15:14:16 -0400

    Add __str defs to api models

--- django/api/models.py
+++ django/api/models.py
@@ -18,9 +18,22 @@ class Restaurant(models.Model):
     loc = models.PointField()
     is_approved = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Rating(models.Model):
     Stars = models.IntegerChoices("star", "one two three four five")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
     rating = models.IntegerField(choices=Stars.choices)
+
+    def __str__(self):
+        rating_string = (
+            str(self.rating)
+            + " stars: "
+            + self.restaurant.name
+            + ", rated by "
+            + self.user.username
+        )
+        return rating_string
