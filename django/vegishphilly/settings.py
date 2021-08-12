83603c21eb74f5c7eb66791c06fecc527b9bcf39
Rachele Morino <rmorino@azavea.com>
2021-08-12 17:11:11 -0400

    Hide Django secret key

--- django/vegishphilly/settings.py
+++ django/vegishphilly/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s*99ujn(=fp%+s$^rq)okgmg)r*ys4n440^-669@m1#hog(n&3"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
