0140546db4636cbdd1cf76fd51949683adbb8a29
Rachele Morino <rmorino@azavea.com>
2021-08-13 16:21:01 -0400

    Adding Simple JWT to Django

--- django/api/models.py
+++ django/api/models.py
@@ -3,14 +3,6 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-"""
-def post_save_receiver(sender, instance, created, **kwargs):
-    pass
-
-
-post_save.connect(post_save_receiver, sender=settings.AUTH_USER_MODEL)
-"""
-
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
--- django/vegishphilly/urls.py
+++ django/vegishphilly/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from . import settings
-
-urlpatterns = [path("admin/", admin.site.urls)] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
 )
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
