a74150791b152058b21df2bbbc229c435114eeb2
Rachele Morino <rmorino@azavea.com>
2021-09-16 16:55:11 -0400

    Update restaurantsFetch types

--- client/src/actions/restaurants.ts
+++ client/src/actions/restaurants.ts
@@ -2,6 +2,6 @@ import { createAction } from "typesafe-actions";
 import { PaginatedRestaurants } from "../models";
 import { LngLatBounds } from "mapbox-gl";
 
-export const restaurantsFetch = createAction("Restaurants Fetch")<[LngLatBounds | null, number]>();
+export const restaurantsFetch = createAction("Restaurants Fetch")<[LngLatBounds, number]>();
 export const restaurantsFetchSuccess = createAction("Restaurants Fetch Success")<PaginatedRestaurants>();
 export const restaurantsFetchFailure = createAction("Restaurants Fetch failure")<string>();
